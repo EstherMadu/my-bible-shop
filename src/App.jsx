47c866fa529cb4119f7cee2ee3388391eb9a8174
esthermadu <prityexsta@gmail.com>
2023-03-26 20:57:08 +0100

    fixed deploy issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Nav } from "./Nav";
 import { Product } from "./Product";
 import { products } from "./products";
-import "./assets/css/app.css";
+import "./assets/css/style.css";
 
 export const App = () => {
   const Cards = products.map((product) => {
